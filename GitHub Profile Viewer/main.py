66a45cb7534c8b09f3a4c1e154b5efd6d179d165
l9tuchiyy <tt3723339@gmail.com>
2026-09-24 21:28:12 +0300

    A few new features in the menu

--- GitHub Profile Viewer/main.py
+++ GitHub Profile Viewer/main.py
@@ -7,9 +7,11 @@ data = response.json()
 if response.status_code == 200:
     print(f'👤{data['name']}\n\n'
           f'Логин: {data['login']}\n'
+          f'Компания: {data['company']}\n'
+          f'Местоположение: {data['location']}\n'
           f'Репозиториев: {data['public_repos']}\n'
           f'Подписчиков: {data['followers']}\n'
           f'Подписок: {data['following']}\n'
-          f'Создан аккаунт: {data['created_at']}')
+          f'Создан аккаунт: {data['created_at'].split('T')[0]}')
 else:
     print(f'Ошибка! [{response.status_code}]')
